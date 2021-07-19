6ef7de5288c23852fa31ada5ed143235ac7c23e1
AnZ4k <77916663+AnZ4k@users.noreply.github.com>
2021-07-18 23:10:21 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,12 @@
 #include "Grade.h"
 
-int main()
+int main ( void )
 {
-    GradeBook myGradeBook1("CCP - C++ Como Programar 5º edição");
-    GradeBook myGradeBook2("teste");
+    GradeBook myGradeBook1 ( "CCP - C++ Como Programar 5º edição" );
+    GradeBook myGradeBook2 ( "teste" );
 
-    myGradeBook1.displayMessage();
-    myGradeBook2.displayMessage();
+    myGradeBook1.displayMessage ( );
+    myGradeBook2.displayMessage ( );
 
     return 0;
 }
