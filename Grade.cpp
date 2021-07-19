4c26d26df0da4782251b37b918db2391da25f063
AnZ4k <77916663+AnZ4k@users.noreply.github.com>
2021-07-18 23:11:55 -0400

    Update Grade.cpp

--- Grade.cpp
+++ Grade.cpp
@@ -4,32 +4,32 @@ using std::endl;
 
 #include "Grade.h"
 
-GradeBook::GradeBook( string name )
+GradeBook::GradeBook ( string name )
 {
-    setCourseName(name);
+    setCourseName ( name );
 }
 
-void GradeBook::setCourseName( string name )
+void GradeBook::setCourseName ( string name )
 {
-    if (name.length() <= 25)
+    if ( name.length ( ) <= 25 )
     {
         courseName = name;
     }
     else
     {
-        courseName = name.substr(0, 25);
+        courseName = name.substr ( 0, 25 );
         cout << "Name \"" << name << "\" Exceeds maximum length (25). \n"
              << "Limiting course name to first 25 characters.\n" << endl;
     }
     
 }
 
-string GradeBook::getCourseName( void )
+string GradeBook::getCourseName ( void )
 {
     return courseName;
 }
 
-void GradeBook::displayMessage( void )
+void GradeBook::displayMessage ( void )
 {
-    cout << "Welcome to " << getCourseName() << " grade book!!" << endl;
+    cout << "Welcome to " << getCourseName ( ) << " grade book!!" << endl;
 }
